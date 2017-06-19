242fd49f3c6dfb4dbd5af925981c7c3eefae8d07
Max <phraine@gmail.com>
2017-06-20 00:19:42 +0200

    Fixed no_cache decorator

--- cyclegan-server/util.py
+++ cyclegan-server/util.py
@@ -3,9 +3,9 @@ from functools import wraps, update_wrapper
 from datetime import datetime
 
 
-def nocache(view):
+def no_cache(view):
     @wraps(view)
-    def no_cache(*args, **kwargs):
+    def no_cache_func(*args, **kwargs):
         response = make_response(view(*args, **kwargs))
         response.headers['Last-Modified'] = datetime.now()
         response.headers['Cache-Control'] = 'no-store, no-cache, \
@@ -14,4 +14,4 @@ must-revalidate, post-check=0, pre-check=0, max-age=0'
         response.headers['Expires'] = '-1'
         return response
 
-    return update_wrapper(no_cache, view)
+    return update_wrapper(no_cache_func, view)
